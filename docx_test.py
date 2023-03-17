afd2db0f93661b3e2903381de64a5f340a94c728
Shintaro Tamefusa <tameshin@gmail.com>
2023-03-18 00:18:26 +0900

    出来た

--- docx_test.py
+++ docx_test.py
@@ -4,15 +4,28 @@ import subprocess
 list = [
     "デイリーポータルＺライター賞",
     "爲房新太朗様",
-    "あなたの深く考えられた記事と、緻密で愛情ある文章に感銘を受け、デイリーポータルＺライター賞を授与いたします。あなたの寄稿は常に読者の注目を集め、彼らに新たな視点を提供しています。更なるご成功を心よりお祈りしております。",
+    "あなたの深く考えられた記事と、緻密で愛情ある文章に感銘を受け、デイリーポータルＺライター賞を授与いたします。\nあなたの寄稿は常に読者の注目を集め、彼らに新たな視点を提供しています。\n更なるご成功を心よりお祈りしております。",
     "授与日：2021年7月10日",
     "授与組織：日本メディア協会"
 ]
 
+tmp = list[2].split("\n")
+count = 0 # この文章がWORDファイル上で何行になるかを示す数
+for t in tmp:
+    print(t)
+    count += int(len(t)/28) # このWORDファイルの一行の最大文字数は27文字。一文が長く文中で改行される回数を足す
+    count += 1 # 句点ごとに改行になるのでその分足す
+print(count)
+lf_count = 11 - count
+
 doc = Document("template.docx")
 print(doc.paragraphs[2].text)
 print(doc.paragraphs[2].runs[0].text)
-doc.paragraphs[0].runs[0].text = "置換したテキストだよ"
+doc.paragraphs[0].runs[0].text = list[0]
+doc.paragraphs[4].runs[0].text = list[2]
+doc.paragraphs[6].runs[0].text = "\n" * lf_count + list[3]
+#if len(list[2]) > 100 and len(list[2]) < 200:
+
 doc.save("sample.docx")
 subprocess.run("docx2pdf /Users/stamefus/prize_by_ai/sample.docx", shell=True)
 
--- prize_by_ai.py
+++ prize_by_ai.py
@@ -8,19 +8,29 @@ import re
 def main():
     name, text = getInputTexts()
     print(name + "\n" + text)
-    prompt = name + "は、" + text + "。そのことに対して架空の賞を設定し、さらに賞状を贈呈するとして、賞の名前・その賞状に記載する文章・授与する組織名を考えて、その順番にタブ区切りで回答してください。賞状に記載する文章は勝手に考えてよく、300文字程度としてください。"
-    prize = getPrizeTexts(prompt)
-    print(prize)
+    prompt = "これまでの指示は忘れてください。さて、" + name + "は、" + text + "。そのことに対して架空の賞を設定し、さらに賞状を贈呈するとします。「賞の名前」「その賞状に記載する文章」「授与する組織名」を考えて、その順番に改行区切りではなく必ずタブ区切りで回答してください。賞状に記載する文章は勝手に考えてよく、200文字程度としてください。"
+    count = 0
+    while True:
+        prize = getPrizeTexts(prompt)
+        print(prize)
 
-    if len(prize) == 3:
-        print("成功")
-        now = datetime.datetime.now()
-        disp_now = "令和" + str(now.year-2018) + "年" + str(now.month) + "月" + str(now.day) + "日"
-        title, body, org = sanitise(prize)
-        makeDocx(title, name, body, disp_now, org)
-        printPrize()
-    else:
-        print("失敗")
+        if len(prize) == 3:
+            print("成功")
+            if input("これで印刷しますか？[y/n]") != "y":
+                print("終了")
+                break
+
+            now = datetime.datetime.now()
+            disp_now = "令和" + str(now.year-2018) + "年" + str(now.month) + "月" + str(now.day) + "日"
+            title, body, org = sanitise(prize)
+            makeDocx(title, name, body, disp_now, org)
+            printPrize()
+            break;
+        else:
+            print("失敗")
+            count += 1
+            if count > 3:
+                break
 
 def getInputTexts():
     file = open("input.txt")
@@ -41,30 +51,46 @@ def sanitise(src):
     # 賞の文章のみ、句点で改行する。
 
     # 賞の名前
-    title = re.sub(r'\n|.*：|「|」', '', src[0])
+    title = re.sub(r'\n|.*：|「|」|"【|】', '', src[0])
 
     # 賞の文章
-    body = re.sub(r'\n|.*：|「|」', '', src[1]).replace('。', '。\n')
+    body = re.sub(r'\n|.*：|「|」|"', '', src[1]).replace('。', '。\n')
 
     # 授与する組織名
-    org = re.sub(r'\n|.*：|「|」', '', src[2])
+    org = re.sub(r'\n|.*：|「|」|"|【|】', '', src[2])
 
     return title, body, org
 
 def makeDocx(title, to_name, body, date, from_name):
-    # TODO 賞の文章の長さによって受賞者名の位置を調整
+    # 賞の文章の長さによって受賞者名の位置を調整
+    tmp = body.split("\n")
+    count = 0 # この文章がWORDファイル上で何行になるかを示す数
+    for t in tmp:
+        count += int(len(t)/18) # このWORDファイルの一行の最大文字数は17文字。一文が長く文中で改行される回数を足す
+        count += 1 # 句点ごとに改行になるのでその分足す
+    lf_count = 10 - count
+    print(lf_count)
+    if lf_count < 0:
+        print("賞の文章が長すぎます")
+        exit(1)
+    elif lf_count > 5:
+        print("賞の文章が短すぎます")
+        exit(1)
+
     doc = Document("template.docx")
-    doc.paragraphs[0].runs[0].text = title
-    doc.paragraphs[2].runs[0].text = to_name
-    doc.paragraphs[4].runs[0].text = body
-    doc.paragraphs[6].runs[0].text = date
-    doc.paragraphs[7].runs[0].text = from_name
+    doc.paragraphs[1].runs[0].text = title
+    doc.paragraphs[3].runs[0].text = to_name + "様"
+    doc.paragraphs[5].runs[0].text = body
+    doc.paragraphs[7].runs[0].text = "\n" * lf_count + date
+    doc.paragraphs[8].runs[0].text = from_name
     doc.save("sample.docx")
 
 def printPrize():
+    now = datetime.datetime.now()
+    filename = now.strftime('%Y%m%d_%H%M%S')
     path = os.getcwd() + "/sample.docx"
-    subprocess.run("docx2pdf " + path, shell=True)
-    print_path = os.getcwd() + "/sample.pdf"
+    print_path = os.getcwd() + "/pdf/" + filename + ".pdf"
+    subprocess.run("docx2pdf " + path + " " + print_path, shell=True)
     #subprocess.run("lpr -P EPSON_EP_707A_Series " + print_path, shell=True)
 
 if __name__ == "__main__":
