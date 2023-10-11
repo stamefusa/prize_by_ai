d47f91f36ad49fa16120992cc6325cc946d50eae
tamefusa shintaro <stamefusa@yahoo.co.jp>
2023-10-12 00:30:01 +0900

    GPT-4に対応

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ file = open(".apikey")
 openai.api_key = file.readline()
 file.close()
 
-order = "爲房新太朗は、本業のWebエンジニアの業務もある中でライターとしてデイリーポータルＺに記事を寄稿しています。そのことに対して架空の賞を設定し、さらに賞状を贈呈するとして、その賞状に記載する文章を考えてください。受賞した理由は勝手に考えて構いません。また、賞状の文章は1行目に賞の名前、2行目に受賞者名、3行目以降に残りの文章を記載するようにしてください。受賞者名の敬称は「様」としてください。末尾には受賞日として今日の日付と、授与する組織名を捏造して記載してください。また「賞の名前」「受賞者名」「受賞日」「授与する組織名」といったラベルは生成する文章には含めず、またプレースホルダとなる要素は必要ありません。例示した文章をそのまま採用しコピー＆ペーストして利用できるようにしてください。"
+order = "爲房新太朗は、本業のWebエンジニアの業務もある中でライターとしてデイリーポータルＺに記事を寄稿しています。そのことに対して架空の賞を設定し、さらに賞状を贈呈するとします。「賞の名前」「その賞状に記載する文章」「授与する組織名」を考えて、json形式で回答してください。jsonのkeyは、「賞の名前」はaward、「その賞状に記載する文章」はtext、「授与する組織名」はorgとしてください。賞状に記載する文章は勝手に考えてよく、200文字程度としてください。"
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": order}])
+#completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": order}])
+completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user", "content": order}],   temperature=1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0)
 print(completion.choices[0].message.content)
